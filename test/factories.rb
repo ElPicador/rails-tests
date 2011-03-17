92c49e9be6c943d4b5c5eb179eb68521c6771d11
Rémy-Christophe Schermesser <git@schermesser.com>
2011-03-17 17:59:22 +0100

    Add tests

---
+++ test/factories.rb
@@ -0,0 +1,4 @@
+Factory.define :user do |u|
+  u.name 'Me Me Me'
+  u.email 'me@me.com'
+end
---
+++ test/unit/user_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+  test "test factories" do
+    Factory :user
+    assert_equal 1, User.all.count
+  end
+
+  test "before create" do
+    user = Factory :user, :email => "toto@toto.com"
+    assert_equal "toto@toto.com", user.email
+
+    user = Factory :user, :email => nil, :name => "titi"
+    assert_equal "titi@me.com", user.email
+  end
+end
